b0e739a2e6b1881ca6517b147f30e281d0fcdb7c
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-01-07 16:38:28 +0900

    Making a JS Clock

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  /*10 미만의 시간일때 0을 붙여주기*/
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+  console.log("ho");
+}
+
+init();
